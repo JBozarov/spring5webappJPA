d155bb8c505c69fba5b9d4d6edce6080cbe687a6
John Bozarov <codinginusa@gmail.com>
2020-08-21 19:54:52 -0400

    Publisher fully implemented

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -1,49 +1,71 @@
 package guru.springframework.spring5webapp.bootstrap;
 
 
-import guru.springframework.spring5webapp.domain.Author;
-import guru.springframework.spring5webapp.domain.Book;
-import guru.springframework.spring5webapp.repositories.AuthorRepository;
-import guru.springframework.spring5webapp.repositories.BookRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 
 @Component
 public class BootStrapData implements CommandLineRunner {
 
-	@Autowired
-	private  AuthorRepository authorRepository; 
-	@Autowired
-	private  BookRepository bookRepository; 
+//	@Autowired
+	private final  AuthorRepository authorRepository; 
+//	@Autowired
+	private final BookRepository bookRepository; 
+	
+	private final PublisherRepository publisherRepository; 
 	
 	
-//	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
-//		this.authorRepository = authorRepository;
-//		this.bookRepository = bookRepository;
-//	}
+	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
+		this.authorRepository = authorRepository;
+		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository; 
+	}
 
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Publisher publisher = new Publisher();
+        publisher.setName("SFG Publishing");
+        publisher.setCity("St Petersburg");
+        publisher.setState("FL");
 
-		Author eric = new Author("Eric", "Evans");
+        publisherRepository.save(publisher); 
+        
+        System.out.println("Publisher count is " + publisherRepository.count());
+        
+        Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123123");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
+        
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd); 
+        
+        
 
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(publisher); 
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "3939459459");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
+        publisher.getBooks().add(noEJB); 
+        
 
         authorRepository.save(rod);
         bookRepository.save(noEJB);
-//
+        publisherRepository.save(publisher); 
+
         System.out.println("Started in Bootstrap");
         System.out.println("Number of Books: " + bookRepository.count());
 	}
