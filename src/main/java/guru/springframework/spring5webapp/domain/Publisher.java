828ce44aaae9b368d76ee6267af582a96d96a799
John Bozarov <codinginusa@gmail.com>
2020-08-21 18:59:18 -0400

    Publisher class initialized

--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -1,11 +1,35 @@
 package guru.springframework.spring5webapp.domain;
 
+import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Publisher {
 
-	private String name; 
-	private Set<Book> books; 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id; 
+	
+	private String name;
+	private String addressLine1; 
+	private String city; 
+	private String state; 
+	private String zip; 
+	
+	
+//	@OneToMany
+//	private Set<Book> books = new HashSet<>();
+
+	public Publisher() {
+		super();
+	} 
+	
 	
 	
 	
